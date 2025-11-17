947e1369f9c7bcf27a4184fe5209f303e50a8a8a
unknown <yuldashevasadbek771@gmail.com>
2025-11-17 13:12:32 +0500

    first commit

---
+++ car_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=120)
+    photo = models.ImageField(upload_to='photos/')
+    color = models.CharField(max_length=15)
+    type = models.CharField(max_length=50)
+    price = models.CharField()
+    is_new = models.CharField()
+
+    def __str__(self):
+        return self.name
+
+
---
+++ car_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import CarListApiView, CarCreateApiView, CarEditApiView, CarDeleteApiView
+
+urlpatterns = [
+    path('', CarListApiView.as_view(), name='cars'),
+    path('cars/', CarListApiView.as_view(), name='cars'),
+    path('cars/create/', CarCreateApiView.as_view(), name='cars_create'),
+    path('cars/edit/<int:pk>/', CarEditApiView.as_view(), name='edit'),
+    path('cars/delete/<int:pk>/', CarDeleteApiView.as_view(), name='car_delete')
+
+]
